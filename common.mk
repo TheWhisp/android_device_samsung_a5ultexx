ed1be30850e47b70b0c0df39d97e919bf4d24945
Davis Malnieks <malnieks.davis@gmail.com>
2015-10-02 17:56:07 +0300

    rootdir: clean-up, add ant+ permission

--- common.mk
+++ common.mk
@@ -22,6 +22,7 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/a5-common/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
