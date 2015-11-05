17ae7abecd8abc0f5c9c4c694b073f4090a3a0c3
Davis Malnieks <daavis.90@gmail.com>
2015-11-05 23:11:18 +0200

    don't use custom mkbootimg.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,7 @@ TARGET_CPU_CORTEX_A53 := true
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/a5-common/include
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK      := device/samsung/a5-common/mkbootimg.mk
+BOARD_DTBTOOL_ARG            := -2
 BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
 BOARD_KERNEL_BASE            := 0x80000000
 BOARD_RAMDISK_OFFSET         := 0x02000000
